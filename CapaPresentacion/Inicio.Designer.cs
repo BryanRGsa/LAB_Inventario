3cfa72b25cd587cf51e92089a9bd70c963c669a4
BryanRGsa <houndeye010@gmail.com>
2023-10-13 09:28:49 -0500

    añadiendo un avance del sistema

---
+++ CapaPresentacion/Inicio.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace CapaPresentacion
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Menu = new System.Windows.Forms.MenuStrip();
+            this.menuprestamo = new FontAwesome.Sharp.IconMenuItem();
+            this.menudevolucion = new FontAwesome.Sharp.IconMenuItem();
+            this.menuinventario = new FontAwesome.Sharp.IconMenuItem();
+            this.menuinforme = new FontAwesome.Sharp.IconMenuItem();
+            this.menuhistorial = new FontAwesome.Sharp.IconMenuItem();
+            this.menumaterialdañado = new FontAwesome.Sharp.IconMenuItem();
+            this.MenuTitulo = new System.Windows.Forms.MenuStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Menu
+            // 
+            this.Menu.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuprestamo,
+            this.menudevolucion,
+            this.menuinventario,
+            this.menuinforme,
+            this.menuhistorial,
+            this.menumaterialdañado});
+            this.Menu.Location = new System.Drawing.Point(0, 72);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(844, 73);
+            this.Menu.TabIndex = 0;
+            this.Menu.Text = "menuStrip1";
+            // 
+            // menuprestamo
+            // 
+            this.menuprestamo.AutoSize = false;
+            this.menuprestamo.IconChar = FontAwesome.Sharp.IconChar.Handshake;
+            this.menuprestamo.IconColor = System.Drawing.Color.Black;
+            this.menuprestamo.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.menuprestamo.IconSize = 50;
+            this.menuprestamo.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.menuprestamo.Name = "menuprestamo";
+            this.menuprestamo.Size = new System.Drawing.Size(80, 69);
+            this.menuprestamo.Text = "Prestamo";
+            this.menuprestamo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.menuprestamo.Click += new System.EventHandler(this.iconMenuItem5_Click);
+            // 
+            // menudevolucion
+            // 
+            this.menudevolucion.AutoSize = false;
+            this.menudevolucion.IconChar = FontAwesome.Sharp.IconChar.HandHoldingHand;
+            this.menudevolucion.IconColor = System.Drawing.Color.Black;
+            this.menudevolucion.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.menudevolucion.IconSize = 50;
+            this.menudevolucion.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.menudevolucion.Name = "menudevolucion";
+            this.menudevolucion.Size = new System.Drawing.Size(80, 69);
+            this.menudevolucion.Text = "Devolucion";
+            this.menudevolucion.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // menuinventario
+            // 
+            this.menuinventario.AutoSize = false;
+            this.menuinventario.IconChar = FontAwesome.Sharp.IconChar.DollyFlatbed;
+            this.menuinventario.IconColor = System.Drawing.Color.Black;
+            this.menuinventario.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.menuinventario.IconSize = 50;
+            this.menuinventario.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.menuinventario.Name = "menuinventario";
+            this.menuinventario.Size = new System.Drawing.Size(80, 69);
+            this.menuinventario.Text = "Inventario";
+            this.menuinventario.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // menuinforme
+            // 
+            this.menuinforme.AutoSize = false;
+            this.menuinforme.IconChar = FontAwesome.Sharp.IconChar.FileEdit;
+            this.menuinforme.IconColor = System.Drawing.Color.Black;
+            this.menuinforme.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.menuinforme.IconSize = 50;
+            this.menuinforme.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.menuinforme.Name = "menuinforme";
+            this.menuinforme.Size = new System.Drawing.Size(80, 69);
+            this.menuinforme.Text = "Informe";
+            this.menuinforme.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // menuhistorial
+            // 
+            this.menuhistorial.AutoSize = false;
+            this.menuhistorial.IconChar = FontAwesome.Sharp.IconChar.FileText;
+            this.menuhistorial.IconColor = System.Drawing.Color.Black;
+            this.menuhistorial.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.menuhistorial.IconSize = 50;
+            this.menuhistorial.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.menuhistorial.Name = "menuhistorial";
+            this.menuhistorial.Size = new System.Drawing.Size(80, 69);
+            this.menuhistorial.Text = "Historial";
+            this.menuhistorial.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // menumaterialdañado
+            // 
+            this.menumaterialdañado.AutoSize = false;
+            this.menumaterialdañado.IconChar = FontAwesome.Sharp.IconChar.FlaskVial;
+            this.menumaterialdañado.IconColor = System.Drawing.Color.Black;
+            this.menumaterialdañado.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.menumaterialdañado.IconSize = 50;
+            this.menumaterialdañado.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.menumaterialdañado.Name = "menumaterialdañado";
+            this.menumaterialdañado.Size = new System.Drawing.Size(96, 69);
+            this.menumaterialdañado.Text = "Material Dañado";
+            this.menumaterialdañado.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // MenuTitulo
+            // 
+            this.MenuTitulo.AutoSize = false;
+            this.MenuTitulo.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.MenuTitulo.Location = new System.Drawing.Point(0, 0);
+            this.MenuTitulo.Name = "MenuTitulo";
+            this.MenuTitulo.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.MenuTitulo.Size = new System.Drawing.Size(844, 72);
+            this.MenuTitulo.TabIndex = 1;
+            this.MenuTitulo.Text = "menuStrip2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(37, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inventario Laboratorio";
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 549);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Menu);
+            this.Controls.Add(this.MenuTitulo);
+            this.MainMenuStrip = this.Menu;
+            this.Name = "Inicio";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            this.Menu.ResumeLayout(false);
+            this.Menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+#pragma warning disable CS0108 // El miembro oculta el miembro heredado. Falta una contraseña nueva
+        private System.Windows.Forms.MenuStrip Menu;
+#pragma warning restore CS0108 // El miembro oculta el miembro heredado. Falta una contraseña nueva
+        private System.Windows.Forms.MenuStrip MenuTitulo;
+        private System.Windows.Forms.Label label1;
+        private FontAwesome.Sharp.IconMenuItem menumaterialdañado;
+        private FontAwesome.Sharp.IconMenuItem menuprestamo;
+        private FontAwesome.Sharp.IconMenuItem menudevolucion;
+        private FontAwesome.Sharp.IconMenuItem menuinventario;
+        private FontAwesome.Sharp.IconMenuItem menuinforme;
+        private FontAwesome.Sharp.IconMenuItem menuhistorial;
+    }
+}
+
---
+++ CapaPresentacion/Login.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace CapaPresentacion
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btningresar = new FontAwesome.Sharp.IconButton();
+            this.btncancelar = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 263);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(48, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Laboratorio";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.FlaskVial;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlLightLight;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 129;
+            this.iconPictureBox1.Location = new System.Drawing.Point(65, 49);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(139, 129);
+            this.iconPictureBox1.TabIndex = 2;
+            this.iconPictureBox1.TabStop = false;
+            this.iconPictureBox1.Click += new System.EventHandler(this.iconPictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(307, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Usuario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(307, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Contraseña";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(310, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(310, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(239, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // btningresar
+            // 
+            this.btningresar.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btningresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btningresar.IconChar = FontAwesome.Sharp.IconChar.DoorOpen;
+            this.btningresar.IconColor = System.Drawing.Color.Black;
+            this.btningresar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btningresar.IconSize = 21;
+            this.btningresar.Location = new System.Drawing.Point(310, 179);
+            this.btningresar.Name = "btningresar";
+            this.btningresar.Size = new System.Drawing.Size(93, 38);
+            this.btningresar.TabIndex = 7;
+            this.btningresar.Text = "Ingresar";
+            this.btningresar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btningresar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btningresar.UseVisualStyleBackColor = false;
+            this.btningresar.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.BackColor = System.Drawing.Color.Tomato;
+            this.btncancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncancelar.IconChar = FontAwesome.Sharp.IconChar.TimesCircle;
+            this.btncancelar.IconColor = System.Drawing.Color.Black;
+            this.btncancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btncancelar.IconSize = 21;
+            this.btncancelar.Location = new System.Drawing.Point(456, 179);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(93, 38);
+            this.btncancelar.TabIndex = 8;
+            this.btncancelar.Text = "Cancelar";
+            this.btncancelar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btncancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btncancelar.UseVisualStyleBackColor = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(593, 263);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.btningresar);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.iconPictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private FontAwesome.Sharp.IconButton btningresar;
+        private FontAwesome.Sharp.IconButton btncancelar;
+    }
+}
